71c8cc905ea456a0f79a443ab16c9f17d58421f9
Jhonatan Isaí Ramírez Álvarez <31527722+JhonatanIsai05@users.noreply.github.com>
2019-08-27 23:49:43 -0500

    Add files via upload

---
+++ tarea 7.py
@@ -0,0 +1,16 @@
+def add( a , b):
+    return a + b
+
+def substract( a , b):
+    return a - b
+
+def multiply( a , b):
+    return a * b
+
+def divide( a , b):
+    return a / b
+
+print (add (3,5))
+print (substract (10,4))
+print (multiply (7, -4))
+print (divide (45,5))
